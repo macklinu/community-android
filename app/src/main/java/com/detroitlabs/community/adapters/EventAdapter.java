7767e32124981584bbf78b7756ea6fae2c17e312
Jeff Sibbold <jsibbold@gmail.com>
2014-10-01 02:41:15 -0400

    problem fragment

---
+++ app/src/main/java/com/detroitlabs/community/adapters/EventAdapter.java
@@ -0,0 +1,37 @@
+package com.detroitlabs.community.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.detroitlabs.community.R;
+import com.detroitlabs.community.model.Event;
+
+import org.joda.time.DateTime;
+
+import java.util.List;
+
+public class EventAdapter extends ArrayAdapter<Event>{
+
+    public EventAdapter(Context context, List<Event> objects){
+        super(context, 0, objects);
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent){
+
+        if (view == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            view = inflater.inflate(R.layout.row_event, null);
+        }
+
+        TextView tv = (TextView)view.findViewById(R.id.eventText);
+
+        tv.setText(new DateTime(getItem(position).getStartTime()).toString("MM d h:mm"));
+
+        return view;
+    }
+}
