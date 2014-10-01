52b11f9e2e6e47728eff14a1e7bd6099e4fb4706
Macklin Underdown <macklin@detroitlabs.com>
2014-10-01 04:45:28 -0400

    login persists

--- app/src/main/java/com/detroitlabs/community/prefs/Prefs.java
+++ app/src/main/java/com/detroitlabs/community/prefs/Prefs.java
@@ -2,7 +2,9 @@ package com.detroitlabs.community.prefs;
 
 import org.androidannotations.annotations.sharedpreferences.SharedPref;
 
-@SharedPref
+import static org.androidannotations.annotations.sharedpreferences.SharedPref.Scope.UNIQUE;
+
+@SharedPref(value = UNIQUE)
 public interface Prefs {
     String user();
 }
