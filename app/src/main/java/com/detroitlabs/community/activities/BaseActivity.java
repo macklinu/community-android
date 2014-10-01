2aa4e3c827b9ff60246b3e325bd05ba3731a4198
Macklin Underdown <macklin@detroitlabs.com>
2014-09-30 23:49:35 -0400

    add #addFragment

--- app/src/main/java/com/detroitlabs/community/activities/BaseActivity.java
+++ app/src/main/java/com/detroitlabs/community/activities/BaseActivity.java
@@ -7,6 +7,7 @@ import android.app.FragmentTransaction;
 import com.detroitlabs.community.R;
 
 public abstract class BaseActivity extends Activity {
+    
     @Override
     public void onBackPressed() {
         if (getFragmentManager().getBackStackEntryCount() != 0) {
@@ -25,6 +26,17 @@ public abstract class BaseActivity extends Activity {
         }
 
         fragmentTransaction.replace(R.id.container, fragment);
+        invalidateOptionsMenu();
+    }
+
+    public void addFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        if (addToBackStack) {
+            fragmentTransaction.addToBackStack(fragment.getClass().getName());
+        }
+
+        fragmentTransaction.add(R.id.container, fragment, fragment.getClass().getName());
         invalidateOptionsMenu();
     }
 }
